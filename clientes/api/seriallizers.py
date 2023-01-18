0e5b016b89ac5f8bf2af0e2a71cb0d6f7202c2ab
Lucas-Fagundes-Nunes <113182658+Lucas-Fagundes-Nunes@users.noreply.github.com>
2023-01-18 15:17:02 -0300

    Update seriallizers.py
    Verificação CPF

--- clientes/api/seriallizers.py
+++ clientes/api/seriallizers.py
@@ -1,6 +1,7 @@
-from rest_framework import serializers
 import requests
 from clientes import models
+from rest_framework import serializers
+
 
 class ClientesSerializers(serializers.ModelSerializer):
     class Meta:
@@ -8,9 +9,16 @@ class ClientesSerializers(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate_cpf(self, cpf):
+        for c in cpf:
+            if (c == "0" or c == '1' or c == '2' or c == '3' or c== '4' or c == '5' or c == '6' or c == '7' or c == '8' or c == '9'):
+                print('É um número: ' + c)
+            else:
+                print('Não é número '+ c)
+                raise serializers.ValidationError("O CPF deve conter apenas números ! Erro no caracter: "+c)
         if len(str(cpf)) != 11:
             raise serializers.ValidationError("O CPF deve ter 11 digitos")
         return cpf
+            
 
 
     def validate_cep(self,  cep):
@@ -18,7 +26,7 @@ class ClientesSerializers(serializers.ModelSerializer):
         if request:
             raise serializers.ValidationError("CEP inválido!")
         return cep
-
+    
     
     def validate_telefone(self, telefone):
         if len(str(telefone)) < 8 or len(str(telefone)) > 13:
